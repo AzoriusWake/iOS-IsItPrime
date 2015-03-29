dae813a863d2b9e4b0444662d70db306f87a01dc
Henry Tam <IamHenry@Henrys-MacBook-Air.local>
2015-03-29 16:24:11 -0700

    third app -learned auto layouts and added some logic

---
+++ Prime Number/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Prime Number
+//
+//  Created by Henry Tam on 3/29/15.
+//  Copyright (c) 2015 Henry Tam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet var number: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var numberInt = number.text.toInt()
+        
+        if numberInt != nil {
+            
+            var unwrappedNumber = numberInt!
+            
+            var isPrime = true
+            
+            if unwrappedNumber == 1 {
+                
+                isPrime = false
+                
+            }
+            
+            if unwrappedNumber != 2 && unwrappedNumber != 1 {
+                
+                for var i = 2; i < unwrappedNumber; i++ {
+                    
+                    if unwrappedNumber % i == 0 {
+                        
+                        isPrime = false
+                        
+                    }
+                    
+                }
+                
+            }
+            
+            if isPrime == true {
+                
+                resultLabel.text = "\(unwrappedNumber) is prime!"
+            
+            } else {
+                
+                resultLabel.text = "\(unwrappedNumber) is not prime!"
+                
+            }
+            
+        } else {
+            
+            resultLabel.text = "Please enter a number."
+            
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
